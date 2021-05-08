1c7230034e2c593671f59f93f386f3419db90aa5
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2021-05-08 10:08:49 +0100

    change dates auto

--- offence/models.py
+++ offence/models.py
@@ -18,8 +18,8 @@ class Offence(models.Model):
     offence = models.ForeignKey(OffenceCategory, on_delete=models.CASCADE)
     location = models.CharField(max_length=255)
     treated = models.BooleanField(default=False)
-    date = models.DateTimeField(auto_now=True, verbose_name='date recorded')
-    updated = models.DateTimeField(auto_now_add=True, verbose_name='last updated')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='date recorded')
+    updated = models.DateTimeField(auto_now=True, verbose_name='last updated')
 
     def __str__(self):
         return self.title
